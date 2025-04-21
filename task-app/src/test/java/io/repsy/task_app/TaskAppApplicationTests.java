99642729bfaf075eb79c34edf409431ef52c5edc
Mustafa Furkan ŞAHİN <76617516+mfurkansahn@users.noreply.github.com>
2025-04-22 02:52:17 +0300

    chore: add Spring Boot project into repository-app module

---
+++ task-app/src/test/java/io/repsy/task_app/TaskAppApplicationTests.java
@@ -0,0 +1,13 @@
+package io.repsy.task_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
